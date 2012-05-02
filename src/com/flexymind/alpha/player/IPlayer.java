1598c42c0ac20282a9815f7b1197254e68198826
Ilya Korytnyy <ilya.korytnyy@gmail.com>
2012-05-03 01:56:53 +0400

    for playing notes

---
+++ src/com/flexymind/alpha/player/IPlayer.java
@@ -0,0 +1,8 @@
+package com.flexymind.alpha.player;
+
+/**
+ * @author Ilya Koritniy
+ */
+public interface IPlayer {
+    public void play(Note note);
+}
---
+++ src/com/flexymind/alpha/player/PianoPlayer.java
@@ -0,0 +1,15 @@
+package com.flexymind.alpha.player;
+
+/**
+ * @author Ilya Koritniy
+ */
+public class PianoPlayer implements IPlayer {
+
+    /**
+     * Gets the .wav file for that Note and plays it.
+     */
+    @Override
+    public void play(Note note) {
+        //
+    }
+}
