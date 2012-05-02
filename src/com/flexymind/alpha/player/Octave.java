34b4bffb9335da52352a303e1cbabb97c7fa1042
Ilya Korytnyy <ilya.korytnyy@gmail.com>
2012-05-03 01:09:59 +0400

    note stuff added

---
+++ src/com/flexymind/alpha/player/Octave.java
@@ -0,0 +1,16 @@
+package com.flexymind.alpha.player;
+
+/**
+ * @author Ilya Koritniy
+ */
+public enum Octave {
+    SUBCONTR,
+    CONRT,
+    BIG,
+    SMALL,
+    FIRST,
+    SECOND,
+    THIRD,
+    FOURTH,
+    FIFTH
+}
