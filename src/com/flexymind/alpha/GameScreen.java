e501c179c91137d01e6eb9419e32d16b131cce00
Ilya Korytnyy <ilya.korytnyy@gmail.com>
2012-05-03 13:13:08 +0400

    comment changed

--- src/com/flexymind/alpha/GameScreen.java
+++ src/com/flexymind/alpha/GameScreen.java
@@ -5,12 +5,13 @@ import android.os.Bundle;
 
 public class GameScreen extends Activity
 {
-    /** Called when the activity is first created. */
+    /**
+     * Called when the activity is first created.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        //
     }
 }
