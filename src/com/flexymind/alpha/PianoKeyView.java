41645295ce299af8a46d7986a6c478c6d9d79f3e
dandandan <nitropolis@gmail.com>
2012-05-06 23:51:16 +0300

    added an open-sourse library for parsing SVG files. also added a class for piano key.

---
+++ src/com/flexymind/alpha/PianoKeyView.java
@@ -0,0 +1,17 @@
+package com.flexymind.alpha;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class PianoKeyView extends View {
+
+    public PianoKeyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PianoKeyView(Context context) {
+        super(context);
+    }
+}
