e7bf0290aad1ccfb5e029daf8bcca6e6bd76a35f
dandandan <nitropolis@gmail.com>
2012-05-07 01:48:25 +0300

    successful show test svg picture

--- src/com/flexymind/alpha/PianoKeyView.java
+++ src/com/flexymind/alpha/PianoKeyView.java
@@ -1,17 +1,37 @@
 package com.flexymind.alpha;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.View;
+import android.app.Activity;
+import android.widget.ImageView;
+import com.larvalabs.svgandroid.SVGParser;
+import com.larvalabs.svgandroid.SVG;
 
-public class PianoKeyView extends View {
+import static com.larvalabs.svgandroid.SVGParser.getSVGFromResource;
+
+public class PianoKeyView extends ImageView {
+    private Drawable pianoKey;
 
     public PianoKeyView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        makeDrawableKey();
     }
 
     public PianoKeyView(Context context) {
         super(context);
+        
+        makeDrawableKey();
     }
+
+    private void makeDrawableKey() {
+        this.setBackgroundColor(Color.WHITE);
+        SVG svg = getSVGFromResource(getResources(),
+                R.raw.bigkey);
+       this.setImageDrawable(svg.createPictureDrawable());
+    }
+
 }
