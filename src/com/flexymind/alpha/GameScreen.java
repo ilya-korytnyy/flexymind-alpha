ea67d188844817901d183de6c2310acd6cd4caa7
pasha010 <mpa026@gmail.com>
2012-05-08 16:11:16 +0400

    Added landskape orientation (user can't change orientation). Added activity lifecircle methods as onStart(), onPause(), onStop(), onResume(), onDestroy. Don't know what about background (picture or solid color).

--- src/com/flexymind/alpha/GameScreen.java
+++ src/com/flexymind/alpha/GameScreen.java
@@ -1,10 +1,10 @@
 package com.flexymind.alpha;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
-public class GameScreen extends Activity
-{
+public class GameScreen extends Activity {
     /**
      * Called when the activity is first created.
      */
@@ -12,5 +12,32 @@ public class GameScreen extends Activity
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
 }
