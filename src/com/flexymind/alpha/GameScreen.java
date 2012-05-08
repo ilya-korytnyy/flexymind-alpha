4baa0e3878d418bc791031a1216d2b7b9ea0dc89
dandandan <nitropolis@gmail.com>
2012-05-09 02:18:16 +0300

    add public fields for screen dimensions using deprecated functions. need to resolve at perspective

--- src/com/flexymind/alpha/GameScreen.java
+++ src/com/flexymind/alpha/GameScreen.java
@@ -2,17 +2,25 @@ package com.flexymind.alpha;
 
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 
 public class GameScreen extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
+    public static Integer windowW = 0;
+    public static Integer windowH = 0;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        windowW = display.getWidth();
+        windowH = display.getHeight();
+
     }
 
     @Override
