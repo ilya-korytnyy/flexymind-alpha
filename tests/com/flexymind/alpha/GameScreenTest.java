338c6fb4299850501c4352db73ae9ccce0e4a0c4
pasha010 <mpa026@gmail.com>
2012-05-09 12:42:59 +0400

    Created JUnit test for class GameScreen

---
+++ tests/com/flexymind/alpha/GameScreenTest.java
@@ -0,0 +1,44 @@
+package com.flexymind.alpha;
+
+import junit.framework.TestCase;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pavel
+ * Date: 09.05.12
+ * Time: 12:41
+ * To change this template use File | Settings | File Templates.
+ */
+public class GameScreenTest extends TestCase {
+    public void setUp() throws Exception {
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testOnCreate() throws Exception {
+
+    }
+
+    public void testOnStart() throws Exception {
+
+    }
+
+    public void testOnPause() throws Exception {
+
+    }
+
+    public void testOnStop() throws Exception {
+
+    }
+
+    public void testOnResume() throws Exception {
+
+    }
+
+    public void testOnDestroy() throws Exception {
+
+    }
+}
