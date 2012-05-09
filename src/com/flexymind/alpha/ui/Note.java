29dbbc18ce409ee98d10f0e66d850b39336a2250
Ilya Korytnyy <ilya.korytnyy@gmail.com>
2012-05-09 10:52:47 +0400

    PIAN-7:classes and layout for drawing noteboard

---
+++ src/com/flexymind/alpha/ui/Note.java
@@ -0,0 +1,28 @@
+package com.flexymind.alpha.ui;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * @author Ilya Koritniy
+ */
+public class Note extends View {
+
+    public Note(Context context) {
+        super(context);
+    }
+
+    public Note(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Note(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public void setColor(Color color) {
+        //
+    }
+}
