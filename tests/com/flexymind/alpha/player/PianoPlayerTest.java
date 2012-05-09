7c789036c80b6a453a654a3f2ceb52e4cbe61eee
pasha010 <mpa026@gmail.com>
2012-05-09 12:42:39 +0400

    Created JUnit test for class PianoPlayer

---
+++ tests/com/flexymind/alpha/player/PianoPlayerTest.java
@@ -0,0 +1,24 @@
+package com.flexymind.alpha.player;
+
+import junit.framework.TestCase;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pavel
+ * Date: 09.05.12
+ * Time: 12:41
+ * To change this template use File | Settings | File Templates.
+ */
+public class PianoPlayerTest extends TestCase {
+    public void setUp() throws Exception {
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testPlay() throws Exception {
+
+    }
+}
