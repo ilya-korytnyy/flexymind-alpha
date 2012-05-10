12a2721ea8f4624ad0fee852d6e2f20cd11cde16
AlexTrask <jmmkey@gmail.com>
2012-05-11 03:38:40 +0400

    Add parser *.mid files. In procces of parsing they give only notes from first octave.

--- src/com/flexymind/alpha/player/Tone.java
+++ src/com/flexymind/alpha/player/Tone.java
@@ -13,5 +13,39 @@ public enum Tone {
     Dz,  // ре диез
     Fz,  // фа диез
     Gz,  // соль диез
-    C1   // до
+    C1,   // до
+    UNKNOW;
+
+    public static Tone getToneById(int id){
+        switch (id){
+            case 57:
+                return Tone.A;
+            case 58:
+                return Tone.Az;
+            case 48:
+                return Tone.C;
+            case 49:
+                return Tone.Cz;
+            case 60:
+                return Tone.C1;
+            case 50:
+                return Tone.D;
+            case 51:
+                return Tone.Dz;
+            case 52:
+                return Tone.E;
+            case 53:
+                return Tone.F;
+            case 54:
+                return Tone.Fz;
+            case 55:
+                return Tone.G;
+            case 56:
+                return Tone.Gz;
+            case 59:
+                return Tone.H;
+            default:
+                return Tone.UNKNOW;
+        }
+    }
 }
