58fa7ac0f326ef8dd84f1d428a37ac8c9bd52276
dandandan <nitropolis@gmail.com>
2012-05-11 22:06:03 +0300

    it's property already set in AndroidManifest

--- src/com/flexymind/alpha/GameScreen.java
+++ src/com/flexymind/alpha/GameScreen.java
@@ -12,7 +12,6 @@ public class GameScreen extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 
     @Override
