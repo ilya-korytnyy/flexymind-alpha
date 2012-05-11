dbed0266779896d92772c4c9da68f17ad0b2913e
Ilya Korytnyy <ilya.korytnyy@gmail.com>
2012-05-12 02:44:06 +0400

    drawing with SVG

--- src/com/flexymind/alpha/customviews/NoteBoard.java
+++ src/com/flexymind/alpha/customviews/NoteBoard.java
@@ -5,27 +5,32 @@ import android.graphics.*;
 import android.util.AttributeSet;
 import android.view.View;
 import com.flexymind.alpha.R;
+import com.flexymind.alpha.player.Tone;
+import com.larvalabs.svgandroid.SVG;
+import com.larvalabs.svgandroid.SVGParser;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
  * All coordinates are in density pixels
  *
- * @author Ilya Koritniy
  */
 public class NoteBoard extends View {
     /** Number of lines of the board. The value is {@value}. */
     private static final int LINE_COUNT = 5;
     /** The maximum number of notes that can be drawn on the notes board. The value is {@value}. */
     private static final int MAX_NOTES = 7;
-    /** A margin from each side of the display. The value is {@value}. */
-    private static final int MARGIN = 30;
-    /** The value is {@value}. */
-    private static final int LINE_THICKNESS = 2;
+
+    /** A margin from each side of the display. */
+    private int margin;
+    /** The horizontal distance between two (@code Notes} */
+    private int hStep;
 
     private Bitmap clef;
     private ArrayList<Note> notes = new ArrayList<Note>(MAX_NOTES);
+    private HashMap<Tone, Integer> noteYMap = new HashMap<Tone, Integer>(MAX_NOTES);
 
     public NoteBoard(Context context) {
         super(context);
@@ -50,36 +55,28 @@ public class NoteBoard extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        // set up for drawing
-        Paint paint = new Paint();
-        paint.setColor(Color.BLACK);
-        paint.setStrokeWidth(LINE_THICKNESS);
+        // set parameters for drawing
+        margin = this.getHeight() / 5;
+        int linesHeight = (this.getHeight() - margin * 2);
+        int linesWidth = (this.getWidth() - margin * 2);
+        hStep = linesWidth / MAX_NOTES;
 
         // draw 5 lines
-        int currentY = MARGIN;
-        int lineMargin = (this.getHeight() - MARGIN * 2) / (LINE_COUNT - 1);
-        for (int i = 0; i < LINE_COUNT; i++) {
-            canvas.drawLine(MARGIN, currentY, this.getWidth() - MARGIN, currentY, paint);
-            currentY += lineMargin;
-        }
+        SVG linesSvg = SVGParser.getSVGFromResource(getResources(), R.raw.linescut);
+        Picture linesPicture = linesSvg.getPicture();
+        canvas.drawPicture(linesPicture, new Rect(margin, margin, linesWidth + margin, linesHeight + margin));
 
-        // draw treble clef
-        float proportion = (float) clef.getHeight() / clef.getWidth();
-
-        // if the bitmap' height is <= then the lines' height, just draw as it is
-        if (clef.getHeight() <= lineMargin * 4) {
-            canvas.drawBitmap(clef, null, new Rect(MARGIN, MARGIN, clef.getWidth(), clef.getHeight()), paint);
-        } else {   // otherwise, scale the bitmap
-            int clefHeight = lineMargin * 4;
-            int clefWidth = (int) (lineMargin * 4 * proportion);
-            canvas.drawBitmap(clef, null, new Rect(MARGIN, MARGIN - LINE_THICKNESS, clefHeight, clefWidth), paint);
-        }
-
-        //HARDCODE FOR NOTE CLASS TEST
-        Note note = new Note(getContext());
-        note.onDraw(canvas, note, MARGIN, lineMargin, 0);
-        //++++++++++++++++++++++++++++
-
+        // draw the clef
+        SVG clefSvg = SVGParser.getSVGFromResource(getResources(), R.raw.goodclef);
+        Picture clefPicture = clefSvg.getPicture();
+        RectF clefLimits = clefSvg.getLimits();
+        float proportion = clefLimits.bottom / clefLimits.right;
+        int clefHeight = linesHeight + margin;
+        int clefLeftIndent = (int) (margin * 1.5);
+        int clefTopIndent = margin / 2;
+        canvas.drawPicture(clefPicture, new RectF(clefLeftIndent, clefTopIndent,
+                                                  clefHeight / proportion + clefLeftIndent,
+                                                  clefHeight + clefTopIndent + margin / 2));
     }
 
     /**
