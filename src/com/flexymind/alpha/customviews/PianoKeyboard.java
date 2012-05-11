ea87fc651685b0fa8d0ce8fcc2158852e34e3c04
dandandan <nitropolis@gmail.com>
2012-05-11 19:41:31 +0300

    added one black key

--- src/com/flexymind/alpha/customviews/PianoKeyboard.java
+++ src/com/flexymind/alpha/customviews/PianoKeyboard.java
@@ -4,11 +4,12 @@ import android.content.Context;
 import android.graphics.Picture;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import com.flexymind.alpha.R;
 import com.larvalabs.svgandroid.SVG;
 import com.larvalabs.svgandroid.SVGParser;
 
-public class PianoKeyboard extends LinearLayout {
+public class PianoKeyboard extends RelativeLayout {
 
     private final static int    COUNT_OF_WHITE_KEYS = 8;
     private              int    keyboardW           = 0;
@@ -33,22 +34,73 @@ public class PianoKeyboard extends LinearLayout {
                              int t, int r, int b) {
         super.onLayout(changed, l, t, r, b);
         addWhiteKeys();
+        addBlackKeys();
+    }
+
+    private void addBlackKeys() {
+        SVG svg = SVGParser.getSVGFromResource(getResources(),
+                R.raw.blackkey);
+
+        RelativeLayout.LayoutParams params = new
+                RelativeLayout.LayoutParams(
+                LayoutParams.FILL_PARENT,
+                LayoutParams.FILL_PARENT);
+
+        int startId = 108;
+
+            params.addRule(RelativeLayout.RIGHT_OF, 100);
+
+
+
+            addKey(svg.getPicture(),
+                    keyboardH / 2,
+                    keyboardW / COUNT_OF_WHITE_KEYS / 2,
+                    params,
+                    startId++);
     }
 
     private void addWhiteKeys() {
         SVG svg = SVGParser.getSVGFromResource(getResources(),
                 R.raw.whitekey);
 
+        RelativeLayout.LayoutParams params = new
+                RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        int startId = 100;
+
+        addKey(svg.getPicture(),
+               keyboardH,
+               keyboardW / COUNT_OF_WHITE_KEYS,
+               params,
+               startId++);
+
+
+
         for (int i = 0; i < COUNT_OF_WHITE_KEYS; i++) {
-            addKey(svg.getPicture());
+
+            RelativeLayout.LayoutParams paramss = new
+                    RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.WRAP_CONTENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+            paramss.addRule(RelativeLayout.RIGHT_OF, startId - 1);
+
+            addKey(svg.getPicture(),
+                    keyboardH,
+                    keyboardW / COUNT_OF_WHITE_KEYS,
+                    paramss,
+                    startId++);
         }
     }
 
-    private void addKey(Picture picture) {
+    private void addKey(Picture picture, int keyH, int keyW, RelativeLayout.LayoutParams params, int id) {
         PianoKey key = new PianoKey(getContext(),
-                                    keyboardH,
-                                    keyboardW / COUNT_OF_WHITE_KEYS,
+                                    keyH,
+                                    keyW,
                                     picture);
-        addView(key);
+        key.setId(id);
+        addView(key, params);
     }
 }
