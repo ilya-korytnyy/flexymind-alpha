59837768c9311392aca14a7f911e71935f4d089e
dandandan <nitropolis@gmail.com>
2012-05-12 17:16:23 +0300

    some refactor

--- src/com/flexymind/alpha/customviews/NoteBoard.java
+++ src/com/flexymind/alpha/customviews/NoteBoard.java
@@ -9,27 +9,24 @@ import com.flexymind.alpha.player.Tone;
 import com.larvalabs.svgandroid.SVG;
 import com.larvalabs.svgandroid.SVGParser;
 
-import java.util.ArrayList;
 import java.util.EnumMap;
-import java.util.HashMap;
-import java.util.List;
 
 /**
  * All coordinates are in density pixels
  */
 public class NoteBoard extends View {
 
-    private static final int ALL_LINES_COUNT = 9;
-    private static final int MAX_NOTES = 7;
-    private static final int widthMargin = 10;
+    private static final int ALL_LINES_COUNT  =  9;
+    private static final int MAX_NOTES        =  7;
+    private static final int WIDTH_MARGIN      = 10;
 
     private static EnumMap<Tone, Integer> noteYCoord;
     private int staveHeight;
     private int staveWidth;
+    private int cliefHeight;
+    private int cliefWidth;
     private int linesGap;
     private int lineHeight;
-    private int clefHeight;
-    private int clefWidth;
     private int notesGap;
 
     public NoteBoard(Context context) {
@@ -49,22 +46,21 @@ public class NoteBoard extends View {
      * @param linesGap gap between two lines of the stave
      */
     private void initializeMap(int linesGap) {
-
         int step = linesGap / 2;
 
-        noteYCoord.put(Tone.C, step*15);
-        noteYCoord.put(Tone.Cz, step*15);
-        noteYCoord.put(Tone.D, step*14);
-        noteYCoord.put(Tone.Dz, step*14);
-        noteYCoord.put(Tone.E, step*13);
-        noteYCoord.put(Tone.F, step*12);
-        noteYCoord.put(Tone.Fz, step*12);
-        noteYCoord.put(Tone.G, step*11);
-        noteYCoord.put(Tone.Gz, step*11);
-        noteYCoord.put(Tone.A, step*10);
-        noteYCoord.put(Tone.Az, step*10);
-        noteYCoord.put(Tone.H, step*7);
-        noteYCoord.put(Tone.C1, step*6);
+        noteYCoord.put(Tone.C,  step * 15);
+        noteYCoord.put(Tone.Cz, step * 15);
+        noteYCoord.put(Tone.D,  step * 14);
+        noteYCoord.put(Tone.Dz, step * 14);
+        noteYCoord.put(Tone.E,  step * 13);
+        noteYCoord.put(Tone.F,  step * 12);
+        noteYCoord.put(Tone.Fz, step * 12);
+        noteYCoord.put(Tone.G,  step * 11);
+        noteYCoord.put(Tone.Gz, step * 11);
+        noteYCoord.put(Tone.A,  step * 10);
+        noteYCoord.put(Tone.Az, step * 10);
+        noteYCoord.put(Tone.H,  step *  7);
+        noteYCoord.put(Tone.C1, step *  6);
     }
 
     /**
@@ -76,42 +72,49 @@ public class NoteBoard extends View {
 
         super.onDraw(canvas);
 
-        //stave size
-        staveHeight = this.getHeight();
-        staveWidth = this.getWidth() - widthMargin*2;
-        //lines gap
-        linesGap = staveHeight / (ALL_LINES_COUNT-1);
-        lineHeight = linesGap / 5;
-        //draw stave (5 lines visible, gap for 2+2 auxiliary lines above&below stave)
-        SVG lineSvg = SVGParser.getSVGFromResource(getResources(), R.raw.line);
-        Picture linesPicture = lineSvg.getPicture();
-        for (int i = 2; i < 7; i++) {
-            canvas.drawPicture(linesPicture, new Rect(widthMargin, i*linesGap,
-                               this.staveWidth, i*linesGap+lineHeight));
-        }
+        setAllNeededSizes();
 
-        //clef size
-        clefHeight = staveHeight;
-        clefWidth = clefHeight / 3;
-        //draw clef
-        SVG clefSvg = SVGParser.getSVGFromResource(getResources(), R.raw.scaledclef);
-        Picture clefPicture = clefSvg.getPicture();
-        canvas.drawPicture(clefPicture, new Rect(widthMargin, 0,
-                           widthMargin+clefWidth, clefHeight));
-
-        //prepare notes coordinates
-        notesGap = (staveWidth-clefWidth) / (MAX_NOTES+1);
-        noteYCoord = new EnumMap<Tone, Integer>(Tone.class);
-        initializeMap(linesGap);
+        drawStave(canvas);
+        drawClief(canvas);
 
         //HARDCODE try to output
         outputNote(canvas, Tone.Fz);
-
     }
 
-    public void outputMelody()
-    {
-        //outputNote();
+    private void setAllNeededSizes() {
+        setStaveSize();
+        setLinesGap();
+        setCliefSize();
+        prepareNotesCoord();
+    }
+
+
+    private void drawStave(Canvas canvas) {
+        SVG lineSvg = SVGParser.getSVGFromResource(getResources(),
+                                                   R.raw.line);
+        Picture linesPicture = lineSvg.getPicture();
+        for (int i = 2; i < 7; i++) {
+            canvas.drawPicture(linesPicture,
+                               new Rect(WIDTH_MARGIN,
+                                       i * linesGap,
+                                       this.staveWidth,
+                                       i * linesGap + lineHeight) );
+        }
+    }
+
+    private void drawClief(Canvas canvas) {
+        SVG clefSvg = SVGParser.getSVGFromResource(getResources(),
+                R.raw.scaledclef);
+        Picture clefPicture = clefSvg.getPicture();
+
+        canvas.drawPicture(clefPicture,
+                           new Rect(WIDTH_MARGIN,
+                           0,
+                           WIDTH_MARGIN + cliefWidth,
+                           cliefHeight) );
+    }
+
+    public void outputMelody() {
     }
 
     /**
@@ -123,46 +126,61 @@ public class NoteBoard extends View {
 
         NoteView note = new NoteView(getContext());
 
-        int x = widthMargin + clefWidth + notesGap;
-        int y = noteYCoord.get(tone) + lineHeight/2;
+        int x = WIDTH_MARGIN + cliefWidth + notesGap;
+        int y = noteYCoord.get(tone) + lineHeight / 2;
         int scale = linesGap;
 
-        //straight or inverted position
-        boolean isStraight = true;
-        if(noteYCoord.get(tone) < noteYCoord.get(Tone.Az)){
-            isStraight = false;
-        }
+        note.onDraw(canvas, x, y, scale, isStraight(tone), isSharp(tone));
+    }
 
-        //has sharp or not
-        boolean isSharp = false;
+    private boolean isStraight(Tone tone) {
+        if(noteYCoord.get(tone) < noteYCoord.get(Tone.Az)){
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isSharp(Tone tone) {
         if (tone== Tone.Cz || tone== Tone.Dz
             || tone== Tone.Fz || tone== Tone.Cz
             || tone== Tone.Gz || tone == Tone.Az) {
-            isSharp = true;
+                return  true;
         }
-
-        note.onDraw(canvas, x, y, scale, isStraight, isSharp);
+        return false;
     }
 
-    /**
-     * Removes all notes
-     */
-    public void clear() {
+    public void removeAllNotes() {
         //
     }
 
-    /**
-     * Changes the color of a note.
-     */
+
     public void highlightCorrectNote(int position) {
        //
     }
 
-    /**
-     * Changes the color of a note.
-     */
     public void highlightErrorNote(Tone tone, int position) {
         //
+    }
+
+    private void prepareNotesCoord() {
+        notesGap = (staveWidth - cliefWidth) / (MAX_NOTES + 1);
+        noteYCoord = new EnumMap<Tone, Integer>(Tone.class);
+        initializeMap(linesGap);
+    }
+
+    private void setCliefSize() {
+        cliefHeight = staveHeight;
+        cliefWidth  = staveHeight  / 3;
+    }
+
+    private void setLinesGap() {
+        linesGap = staveHeight / (ALL_LINES_COUNT - 1);
+        lineHeight = linesGap / 5;
+    }
+
+    private void setStaveSize() {
+        staveHeight = this.getHeight();
+        staveWidth = this.getWidth() - WIDTH_MARGIN * 2;
     }
 
 }
