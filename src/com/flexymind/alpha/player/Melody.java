934dddc0a5e25a4b516d457d2e71c588e714f9ce
Daria Kokareva <kokareva.daria@gmail.com>
2012-05-18 14:33:16 +0400

    Melody

--- src/com/flexymind/alpha/player/Melody.java
+++ src/com/flexymind/alpha/player/Melody.java
@@ -1,6 +1,16 @@
 package com.flexymind.alpha.player;
 
+import com.flexymind.alpha.parsers.MIDIParser;
+import java.io.File;
+import java.util.List;
+
+
 public class Melody {
 
+    public List<MidiNote> melodyNotes;
 
+    public Melody(File melodyFile){
+
+        melodyNotes = MIDIParser.parse(melodyFile);
+    }
 }
