8b6001621c4110df53da188cbbe713473c7ae4e9
pasha010 <mpa026@gmail.com>
2012-05-18 16:06:01 +0400

    Deleted unused methods and change ID of start button. Everything all right.

--- src/com/flexymind/alpha/StartGameScreen.java
+++ src/com/flexymind/alpha/StartGameScreen.java
@@ -2,34 +2,31 @@ package com.flexymind.alpha;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Picture;
 import android.os.Bundle;
 import android.view.View;
 import com.flexymind.alpha.startbutton.StartButton;
 
-public class StartGameScreen extends Activity implements View.OnClickListener{
-    private Picture picture;
+public class StartGameScreen extends Activity implements View.OnClickListener {
+
     private StartButton startButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start);
-        startButton = (StartButton) findViewById(R.id.btnTestID);
+
+        startButton = (StartButton) findViewById(R.id.startbutton);
         startButton.setOnClickListener(this);
+
         StaticResources.res = getResources();
     }
 
     public void createGameScreen() {
         Intent intent = new Intent(this, GameScreen.class);
-       // intent.setClass();
         startActivity(intent);
 		finish();
     }
 
-    private void changeStartButtonPicture() {
-
-    }
 
     @Override
     public void onClick(View view) {
