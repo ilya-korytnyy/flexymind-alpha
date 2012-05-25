ace1fa3c92d15331d0032f623a7ffda327f60fe9
pasha010 <mpa026@gmail.com>
2012-05-25 22:42:36 +0400

    change GameScreen

--- src/com/flexymind/alpha/GameScreen.java
+++ src/com/flexymind/alpha/GameScreen.java
@@ -6,7 +6,7 @@ import com.flexymind.alpha.Game.Game;
 import com.flexymind.alpha.customviews.NoteBoard;
 
 public class GameScreen extends Activity {
-
+    Game game;
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
@@ -17,8 +17,13 @@ public class GameScreen extends Activity {
         NoteBoard noteBoard = (NoteBoard)
                             findViewById(R.id.noteboard);
 
-        Game game = new Game(noteBoard);
-        game.gameStart();
+        game = new Game(noteBoard);
+//        game.gameStart();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();    //To change body of overridden methods use File | Settings | File Templates.
+        game.gameStart();
+    }
 }
