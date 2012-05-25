6a8a878a39d71b4bded71920a4dfcee5aa764821
pasha010 <mpa026@gmail.com>
2012-05-25 23:42:59 +0400

    commit

--- src/com/flexymind/alpha/Game/Game.java
+++ src/com/flexymind/alpha/Game/Game.java
@@ -35,7 +35,7 @@ public class Game {
     private void showIntroduceDialog() {
         StartGameDialog startGameDialog =
                 new StartGameDialog( noteBoard.getContext()
-                        , noteBoard);
+                                    , noteBoard);
         startGameDialog.show();
     }
 
